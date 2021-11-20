931c2ff8db0e92e90527c65e06de243f3b08bf13
Chris Givens <givenscj@Hotmail.com>
2021-11-20 09:07:28 -0800

    cjg init

---
+++ Artifacts/post-install-script01.ps1
@@ -0,0 +1,77 @@
+#Disable-InternetExplorerESC
+function DisableInternetExplorerESC
+{
+  $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+  $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+  Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+  Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green -Verbose
+}
+
+#Enable-InternetExplorer File Download
+function EnableIEFileDownload
+{
+  $HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+  $HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+  Set-ItemProperty -Path $HKLM -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKCU -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKLM -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKCU -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+}
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+mkdir "c:\temp" -ea SilentlyContinue;
+mkdir "c:\labfiles" -ea SilentlyContinue;
+
+#download the solliance pacakage
+$WebClient = New-Object System.Net.WebClient;
+$WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/common-workshop/main/scripts/common.ps1","C:\LabFiles\common.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/common-workshop/main/scripts/httphelper.ps1","C:\LabFiles\httphelper.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/common-workshop/main/scripts/rundeployment.ps1","C:\LabFiles\rundeployment.ps1")
+
+#run the solliance package
+. C:\LabFiles\Common.ps1
+. C:\LabFiles\HttpHelper.ps1
+
+Set-Executionpolicy unrestricted -force
+
+DisableInternetExplorerESC
+
+EnableIEFileDownload
+
+InstallChocolaty
+
+InstallAzPowerShellModule
+
+InstallChrome
+
+InstallNotepadPP
+
+InstallGit
+        
+InstallAzureCli
+
+InstallOffice
+
+InstallMySql
+
+Uninstall-AzureRm -ea SilentlyContinue
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+cd "c:\labfiles";
+
+$branchName = "main";
+$workshopName = "microsoft-mysql-developer-guide";
+$repoUrl = "solliancenet/$workshopName";
+
+
+#download the git repo...
+Write-Host "Download Git repo." -ForegroundColor Green -Verbose
+git clone https://github.com/solliancenet/$workshopName.git $workshopName
+
+Stop-Transcript
