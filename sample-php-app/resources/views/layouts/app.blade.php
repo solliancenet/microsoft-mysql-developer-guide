678265ed85bea1c275eef0c2281a0c30ea79f900
Darren Beckett <>
2021-12-30 10:28:31 -0800

    small tweaks

--- sample-php-app/resources/views/layouts/app.blade.php
+++ sample-php-app/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-		<title>{{ config('app.name', '') }}</title>
+		<title>{{ config('app.name', 'Contoso NoshNow') }}</title>
 
 		<!-- Scripts -->
 		<script src="{{ asset('js/app.js') }}"></script>
