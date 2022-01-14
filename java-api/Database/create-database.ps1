9a8a47936b563c77ccabc6e7b330665b4498679b
saimachi <saikmachi@gmail.com>
2022-01-13 21:44:36 -0800

    Database creation automated

---
+++ java-api/Database/create-database.ps1
@@ -0,0 +1,22 @@
+param([string]$suffix, [string]$password, [string]$resourceGroup, [string]$location)
+
+$serverName = "mysqlflexapp$suffix"
+
+az mysql flexible-server create `
+    --admin-user AppAdmin `
+    --admin-password $password `
+    --database-name app `
+    --location $location `
+    --resource-group $resourceGroup `
+    --name $serverName `
+    --public-access 0.0.0.0 `
+    --tier Burstable `
+    --sku-name Standard_B1ms `
+    --version 8.0.21
+
+az mysql flexible-server execute `
+    -n $serverName `
+    -u AppAdmin `
+    -p $password `
+    -d app `
+    -f './app-database.sql'
