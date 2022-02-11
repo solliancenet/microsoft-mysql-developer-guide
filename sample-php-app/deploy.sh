38783d6ce396e1efca5aa24ce95b87a57366a1c7
$conferencedemo-90 <tim@tailoredtechsolutions.com>
2022-02-10 18:27:27 -0800

    Updated deployment script

--- sample-php-app/deploy.sh
+++ sample-php-app/deploy.sh
@@ -1,5 +1,5 @@
 echo "Running the PHP Sample App deployment scripts"
-mv .env.example .env
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
@@ -7,3 +7,5 @@ php -r "unlink('composer-setup.php');"
 
 echo "Copying files from /home/site/repository/* to /home/site/wwwroot"
 cp -r /home/site/repository/* /home/site/wwwroot
+cp /home/site/repository/.env.example /home/site/wwwroot/.env
+cp /home/site/repository/htaccess /home/site/wwwroot/.htaccess
